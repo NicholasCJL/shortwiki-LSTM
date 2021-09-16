e6133b56e3a47fc550469cdff573aaa538e420ac
Nicholas <chong.jiale.nicholas@gmail.com>
2021-09-16 18:49:59 +0800

    Initial commit

--- RNN_training.py
+++ RNN_training.py
@@ -4,6 +4,9 @@ import tensorflow as tf
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dropout
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
 from keras import backend as K
 import pickle
 
@@ -18,22 +21,68 @@ DECAY_RATE = 0.97
 HIDDEN_LAYER_SIZE = 256
 CELLS_SIZE = 2
 
+# train/test split (0-1)
+train_portion = 0.8
+
 # prefix for saved files
 prefix = "shortwiki"
 
-require_data = True
+require_data = False
 
 if require_data:
     # load and process dataset
     dir = "input.txt"
     char_data = dp.get_data(dir) # get data as array of individual characters
-    print(f"Char data obtained with character set: {sorted(list(set(char_data)))}")
+
+    translator = dp.Translator(char_data)
+
+    print(f"Vocabulary: {translator.vocab}")
+    print(f"Num chars in vocabulary: {len(translator.c2i)}")
+
+    int_data = translator.char_to_int(char_data) # translate characters into list of ints
+    print('Sequence translated.')
+
     seq_data_context, seq_data_answer =\
-        dp.split_sequence(char_data, SEQUENCE_LENGTH) # split data into sequences of context and answers
+        dp.split_sequence(int_data, SEQUENCE_LENGTH, alert=1000000) # split data into sequences of context and answers
 
     # save processed data
     with open(f'{prefix}_data_context_{SEQUENCE_LENGTH}.pkl', 'wb') as file:
         pickle.dump(seq_data_context, file)
+        print('Context saved.')
 
     with open(f'{prefix}_data_answer_{SEQUENCE_LENGTH}.pkl', 'wb') as file:
         pickle.dump(seq_data_answer, file)
+        print('Answers saved.')
+
+    with open(f'{prefix}_translator.pkl', 'wb') as file:
+        pickle.dump(translator, file)
+        print('Translator saved.')
+
+else:
+    # load data from file
+    with open(f'{prefix}_data_context_{SEQUENCE_LENGTH}.pkl', 'rb') as file:
+        seq_data_context = pickle.load(file)
+        print('Context loaded.')
+
+    with open(f'{prefix}_data_answer_{SEQUENCE_LENGTH}.pkl', 'rb') as file:
+        seq_data_answer = pickle.load(file)
+        print('Answers loaded.')
+
+    with open(f'{prefix}_translator.pkl', 'rb') as file:
+        translator = pickle.load(file)
+        print('Translator loaded.')
+
+
+# split dataset into train and test
+n_samples = len(seq_data_context)
+n_train = int(0.8 * n_samples)
+x_train, x_test = seq_data_context[:n_train], seq_data_context[n_train:]
+y_train, y_test = seq_data_answer[:n_train], seq_data_answer[n_train:]
+
+print(f'Num samples: {n_samples}')
+print(f'Num train: {len(x_train)}')
+print(f'Num test: {len(y_test)}')
+
+# # define model
+# model = Sequential
+# model.add(LSTM(HIDDEN_LAYER_SIZE))
--- data_processing.py
+++ data_processing.py
@@ -1,5 +1,25 @@
 import numpy as np
 
+class Translator:
+    def __init__(self, sequence):
+        self.sequence = sequence
+        self.vocab, self.c2i, self.i2c = self.get_vocab()
+
+    def char_to_int(self, sequence):
+        int_sequence = [self.c2i[c] for c in sequence]
+        return int_sequence
+
+    def int_to_char(self, sequence):
+        char_sequence = [self.i2c[i] for i in sequence]
+        return char_sequence
+
+    # get vocabulary and form mapping
+    def get_vocab(self):
+        chars = sorted(list(set(self.sequence)))
+        translator_c2i = {c: i for (i, c) in enumerate(chars)}
+        translator_i2c = {i: c for (i, c) in enumerate(chars)}
+        return chars, translator_c2i, translator_i2c
+
 # obtain dataset and split into character-wise array
 def get_data(dir):
     char_data = []
@@ -9,8 +29,9 @@ def get_data(dir):
     return char_data
 
 # split dataset into samples with n context steps and 1 answer
-def split_sequence(sequence, n):
+def split_sequence(sequence, n, alert=999999999999999999):
     x, y = [], []
+    i = 1
     for i in range(len(sequence)):
         # check if reached the end
         end_ix = i + n
@@ -19,5 +40,8 @@ def split_sequence(sequence, n):
         seq_x, seq_y = sequence[i:end_ix], sequence[end_ix]
         x.append(seq_x)
         y.append(seq_y)
+        if i % alert == 0:
+            print(f"Number of samples processed: {i}")
     print(f"Number of samples: {len(x)}")
     return np.asarray(x), np.asarray(y)
+
