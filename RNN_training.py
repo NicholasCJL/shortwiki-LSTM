4e2d8f1a9546d3b1c744ad8a2ea5a93f064bb35a
Nicholas <chong.jiale.nicholas@gmail.com>
2021-09-16 16:37:52 +0800

    Initial commit

---
+++ RNN_training.py
@@ -0,0 +1,41 @@
+import numpy as np
+import data_processing as dp
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from keras import backend as K
+import pickle
+
+# remove GPU from tensorflow visibility, set device to CPU (use for small networks)
+# tf.config.set_visible_devices([], 'GPU')
+
+# Hyperparameters
+BATCH_SIZE = 32
+SEQUENCE_LENGTH = 50
+LEARNING_RATE = 0.01
+DECAY_RATE = 0.97
+HIDDEN_LAYER_SIZE = 256
+CELLS_SIZE = 2
+
+# prefix for saved files
+prefix = "shortwiki"
+
+# load and process dataset
+dir = "input.txt"
+char_data = dp.get_data(dir) # get data as array of individual characters
+seq_data_context, seq_data_answer =\
+    dp.split_sequence(char_data, SEQUENCE_LENGTH) # split data into sequences of context and answers
+
+# save processed data
+with open(f'{prefix}_data_context_{SEQUENCE_LENGTH}.pkl', 'wb') as file:
+    pickle.dump(seq_data_context, file)
+
+with open(f'{prefix}_data_answer_{SEQUENCE_LENGTH}.pkl', 'wb') as file:
+    pickle.dump(seq_data_answer, file)
+
+print(len(char_data))
+print(len(seq_data_context))
+print(len(seq_data_answer))
+print(len(seq_data_context[0]))
+print(len(seq_data_answer[0]))
